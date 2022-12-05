36081d2ae1e1ac52cb0053e03827ceb5e157c4d5
LEEHM97 <gmlkd2006@gmail.com>
2022-12-05 09:55:27 +0900

    faet: albumentations

---
+++ transforms.py
@@ -0,0 +1,23 @@
+import albumentations as A
+import cv2
+from albumentations.pytorch.transforms import ToTensorV2
+
+def make_transform():
+    base_transform = [
+
+    ]
+
+    train_transform= [
+        A.HorizontalFlip(p=0.5),
+        A.VerticalFlip(p=0.5),
+        A.ShiftScaleRotate(shift_limit=0.2, scale_limit=0.2, rotate_limit=10, 
+                        border_mode=cv2.BORDER_CONSTANT, p=0.5), 
+        A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, brightness_by_max=True, p=0.5),
+    ]
+
+    train_transform.extend(base_transform)
+
+    train_transform = A.Compose(train_transform)
+    test_transform = A.Compose(base_transform)
+    
+    return train_transform, test_transform
