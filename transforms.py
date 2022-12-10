1d0c0157fed08b0040e7f66ee5a62fd3d313693e
LEEHM97 <gmlkd2006@gmail.com>
2022-12-10 14:46:35 +0000

    del: shiftscalerotate

--- transforms.py
+++ transforms.py
@@ -10,9 +10,10 @@ def make_transform():
     train_transform= [
         A.HorizontalFlip(p=0.5),
         A.VerticalFlip(p=0.5),
-        A.ShiftScaleRotate(shift_limit=0.2, scale_limit=0.2, rotate_limit=10, 
-                        border_mode=cv2.BORDER_CONSTANT, p=0.5), 
+        # A.ShiftScaleRotate(shift_limit=0.2, scale_limit=0.2, rotate_limit=10, 
+        #                 border_mode=cv2.BORDER_CONSTANT, p=0.5), 
         A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, brightness_by_max=True, p=0.5),
+        A.CLAHE(clip_limit=2, tile_grid_size=(8, 8), p=0.5),
         A.RandomCrop(320, 320),
     ]
 
